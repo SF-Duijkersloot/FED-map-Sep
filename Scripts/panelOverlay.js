73dc6ec3a2bed710efd2b56f8aa75e8bca081a1d
Sep Duijkersloot <43574905+SF-Duijkersloot@users.noreply.github.com>
2024-01-28 19:29:46 +0100

    Update panelOverlay.js

--- Scripts/panelOverlay.js
+++ Scripts/panelOverlay.js
@@ -1,3 +1,6 @@
+// Code voor de panel heading change, maar dit is een fix voor de laatste panel. Het gaat om de padding van de header op de laaste panel.
+// Bij deze oplossing voeg ik simpelweg wat padding toe. Op de originele site, gebruiken ze een rare manier met fixed position etc. Deze oplossing werkt simpeler.
+
 document.addEventListener("DOMContentLoaded", function () {
   const panelOverlays = document.querySelectorAll(".panel-overlay");
   const lastPanelOverlay = panelOverlays[panelOverlays.length - 1];
