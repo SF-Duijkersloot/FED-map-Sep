c2712f8389534e35d8345222ae80cdb674640e40
Sep Duijkersloot <43574905+SF-Duijkersloot@users.noreply.github.com>
2024-01-28 19:52:58 +0100

    Update scrollNavigation.js

--- Scripts/scrollNavigation.js
+++ Scripts/scrollNavigation.js
@@ -1,22 +1,25 @@
-// Navigation bar scroll
+// De site heeft 2 navigation bars. De één is de standard, deze zie je als je aan de bovenkant van de pagina zit
+// Daarnaast is een een alternate navbar. Deze is te zien als je omhoog scrolled
+
+// scroll position aanmaken
 let prevScrollPos = window.scrollY;
 
-// const regularNav = document.querySelector('.regular-nav');
+// const regularNav = document.querySelector('.regular-nav'); // grayed out omdat regularNav al in de mobileMenu code wordt aangemaakt. dit staat hier voor de context
 const alteredNav = document.querySelector('.altered-nav');
 
 window.addEventListener('scroll', function() {
-    const currentScrollPos = window.scrollY;
+    const currentScrollPos = window.scrollY; //huidige scroll position bijhouden
 
     if (currentScrollPos === 0) {
-        // Top of screen
-        regularNav.style.transform = 'translateY(0)';
-        alteredNav.style.transform = 'translateY(-100%)';
+        // bovenkant van de pagina
+        regularNav.style.transform = 'translateY(0)'; // naar standaar pos. zetten
+        alteredNav.style.transform = 'translateY(-100%)'; // altered nav uit de weg schuiven
     } else if (prevScrollPos > currentScrollPos) {
-        // Scrolling up
+        // omhoog scrollen
         regularNav.style.transform = 'translateY(-100%)';
         alteredNav.style.transform = 'translateY(0)';
     } else {
-        // Scrolling down
+        // naar beneden scrollen
         regularNav.style.transform = 'translateY(-100%)';
         alteredNav.style.transform = 'translateY(-100%)';
     }
